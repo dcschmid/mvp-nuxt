7a39e69a5d514c47b60762717703cc35a2d689af
Daniel Schmid <daniel.schmid@mailbox.org>
2022-12-08 18:21:42 +0100

    loading in course data and deps upgrades

--- pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
+++ pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
@@ -1,6 +1,23 @@
 <template>
   <h2>Lesson</h2>
   <p>This is a lesson</p>
-  <p>Chapter Slug: {{ $route.params.chapterSlug }}</p>
-  <p>Lesson Slug: {{ $route.params.lessonSlug }}</p>
+  <p>{{ chapter.title }}</p>
+  <p>{{ lesson.title }}</p>
 </template>
+
+<script setup>
+const course = useCourse()
+const route = useRoute()
+
+const chapter = computed(() => {
+  return course.chapters.find(
+    (chapter) => chapter.slug === route.params.chapterSlug
+  )
+})
+
+const lesson = computed(() => {
+  return chapter.value.lessons.find(
+    (lesson) => lesson.slug === route.params.lessonSlug
+  )
+})
+</script>
