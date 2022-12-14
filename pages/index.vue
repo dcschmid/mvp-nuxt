b3dccecfcf940fe2eb1e3b2e7cc02456d6cd9fb3
Daniel Schmid <daniel.schmid@mailbox.org>
2022-12-14 13:08:08 +0100

    Creating a Login Page

--- pages/index.vue
+++ pages/index.vue
@@ -1,1 +1,9 @@
-<template>Hello World</template>
+<template>
+  <NuxtLink class="underline font-medium" :href="firstLesson.path"
+    >Go to first lesson</NuxtLink
+  >
+</template>
+
+<script setup>
+const firstLesson = useFirstLesson()
+</script>
